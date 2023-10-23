257bdd8d9c8ee4622911db39ba3b0625dce99f86
leloufadel <leloufadel@gmail.com>
2023-10-23 23:31:28 +0300

    Implement Home Page view fellowing design requirement

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/splash'
   # get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
-  devise_for :users
-
   root to: 'static_pages#splash'
-
+  devise_for :users
   # resources :users, only: [:index, :show, :new] do
   #   resources :categories, only: [:new, :create, :show, :index, :destroy]
   #   resources :purchases, only: [:new, :create, :index, :show, :destroy]
