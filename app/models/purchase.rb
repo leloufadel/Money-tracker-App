e46e0e6a4866972c602cfa465a7e1483c6e174e8
leloufadel <leloufadel@gmail.com>
2023-10-23 14:25:34 +0300

    Add validation to name icon and amount

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,11 @@
 class Purchase < ApplicationRecord
-  belongs_to :user
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  
+
+  has_many :category_purchases
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :categories, through: :category_purchases
 end
+
+
