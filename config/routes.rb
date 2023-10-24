738e066458b5ed99419b83eb31d2d684c0ccce4b
leloufadel <leloufadel@gmail.com>
2023-10-24 17:34:18 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
+# Rails.application.routes.draw do
+  
+#   get 'static_pages/splash'
+#   # get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
+#   root to: 'static_pages#splash'
+#   devise_for :users
+#   resources :users, only: [:index, :show, :new] do
+#     resources :categories, only: [:new, :create, :show, :index, :destroy]
+#   #   resources :purchases, only: [:new, :create, :index, :show, :destroy]
+#   end
+# end
+
 Rails.application.routes.draw do
-  get 'static_pages/splash'
-  # get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
   root to: 'static_pages#splash'
+  get 'static_pages/splash'
+
   devise_for :users
   # resources :users, only: [:index, :show, :new] do
-  #   resources :categories, only: [:new, :create, :show, :index, :destroy]
-  #   resources :purchases, only: [:new, :create, :index, :show, :destroy]
+  resources :categories, only: [:new, :index, :show, :create]
   # end
 end
 
