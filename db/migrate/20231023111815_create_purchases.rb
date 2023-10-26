e05886719979aab626ac05b137fe21ff4138be1a
leloufadel <leloufadel@gmail.com>
2023-10-26 05:54:49 +0300

    update migration

--- db/migrate/20231023111815_create_purchases.rb
+++ db/migrate/20231023111815_create_purchases.rb
@@ -3,8 +3,6 @@ class CreatePurchases < ActiveRecord::Migration[7.0]
     create_table :purchases do |t|
       t.string :name
       t.decimal :amount
-      t.references :user, null: false, foreign_key: true
-
       t.timestamps
     end
   end
